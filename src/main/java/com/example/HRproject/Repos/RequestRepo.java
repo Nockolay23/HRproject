a7da6a90d721aa098c37339619bf044c64df1633
HappyRogue36 <vartemiy334@gmail.com>
2022-06-28 10:01:42 +0300

    Добавил запрос заявок к бд

--- src/main/java/com/example/HRproject/Repos/RequestRepo.java
+++ src/main/java/com/example/HRproject/Repos/RequestRepo.java
@@ -1,7 +1,11 @@
 package com.example.HRproject.Repos;
 
 import com.example.HRproject.domain.Request;
+import com.example.HRproject.domain.StatusRequest;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface RequestRepo extends CrudRepository<Request, Integer> {
+    List<Request> findAllByRequestStatus(StatusRequest requestStatus);
 }
